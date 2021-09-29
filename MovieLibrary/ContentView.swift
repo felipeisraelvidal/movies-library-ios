69b094f009d921a6a465274c186b8d2f96912a69
Felipe Vidal <felipeisra.vidal@gmail.com>
2021-09-28 23:58:03 -0300

    Initial Commit

---
+++ MovieLibrary/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieLibrary
+//
+//  Created by Felipe Israel on 28/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieLibrary/MovieLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieLibraryApp.swift
+//  MovieLibrary
+//
+//  Created by Felipe Israel on 28/09/21.
+//
+
+import SwiftUI
+
+@main
+struct MovieLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
